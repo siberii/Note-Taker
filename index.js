9255da206eaa5a260f68039bbd330e5bf642cc68
Simon Berhanu <simon1515@hotmail.com>
2019-08-02 22:02:39 -0400

    Add auto resize to textarea

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ buttonTab.addEventListener("click", () => {
 
 const buttonNote = document.querySelector(".btn-note-add");
 buttonNote.addEventListener("click", () => {
-  createNoteCard();
+  createNoteCreator();
 });
 
 
@@ -17,6 +17,7 @@ for (const tab of tabs) {
   });
 }
 
+
 const groups = document.getElementsByClassName("group");
 for (const group of groups) {
   group.addEventListener("click", () => {
@@ -24,6 +25,49 @@ for (const group of groups) {
   });
 }
 
+
+const cardNote = document.querySelector(".card");
+cardNote.addEventListener("click", (e) => {
+  createNoteEditor(e);
+});
+
+
+function createNoteEditor(e) {
+  const target = e.currentTarget || e.srcElement;
+  // Create note elements
+  const noteTitle = document.createElement("input");
+  noteTitle.classList.add("note-title");
+  noteTitle.value = target.querySelector(".card-title").innerText;
+  noteTitle.placeholder = "Title";
+  const noteContent = document.createElement("textarea");
+  noteContent.classList.add("note-content");
+  noteContent.value = target.querySelector(".card-content").innerText;
+  noteContent.placeholder = "Take a note...";
+  noteContent.autofocus = true;
+  
+  noteContent.addEventListener("keydown", () => {
+    autoGrow(noteContent);
+  });
+
+  const buttonCreatorNote = document.createElement("button");
+  buttonCreatorNote.classList.add("btn-creator-add-note");
+  buttonCreatorNote.innerText = "Save";
+  buttonCreatorNote.addEventListener("click", () => {
+    target.querySelector(".card-title").innerText = noteTitle.value;
+    target.querySelector(".card-content").innerText = noteContent.value;
+    document.querySelector(".overlay-note-creator").remove();
+    target.scrollIntoView();
+  });
+  const overlayNoteCreator = document.createElement("div");
+  overlayNoteCreator.classList.add("overlay-note-creator");
+  overlayNoteCreator.appendChild(noteTitle);
+  overlayNoteCreator.appendChild(noteContent);
+  overlayNoteCreator.appendChild(buttonCreatorNote);
+  const body = document.querySelector("body");
+  body.appendChild(overlayNoteCreator);
+
+  autoGrow(noteContent);
+}
 
 function createTab() {
   const div = document.createElement("div");
@@ -36,6 +80,35 @@ function createTab() {
   sections.appendChild(div);
 }
 
+function createNoteCreator() {
+  // Create note elements
+  const noteTitle = document.createElement("input");
+  noteTitle.classList.add("note-title");
+  noteTitle.placeholder = "Title";
+
+  const noteContent = document.createElement("textarea");
+  noteContent.classList.add("note-content");
+  noteContent.placeholder = "Take a note...";
+  noteContent.autofocus = true;
+  noteContent
+
+  const buttonCreatorNote = document.createElement("button");
+  buttonCreatorNote.classList.add("btn-creator-add-note");
+  buttonCreatorNote.innerText = "Add new note";
+  buttonCreatorNote.addEventListener("click", () => {
+    createNoteCard();
+  });
+
+  const overlayNoteCreator = document.createElement("div");
+  overlayNoteCreator.classList.add("overlay-note-creator");
+  overlayNoteCreator.appendChild(noteTitle);
+  overlayNoteCreator.appendChild(noteContent);
+  overlayNoteCreator.appendChild(buttonCreatorNote);
+
+  const body = document.querySelector("body");
+  body.appendChild(overlayNoteCreator);
+}
+
 function createNoteCard() {
   const card = document.createElement("div");
   card.classList.add("card");
@@ -44,15 +117,24 @@ function createNoteCard() {
   date.innerText = "July 26, 2019";
   const title = document.createElement("div");
   title.classList.add("card-title");
-  title.innerText = "Untitled";
+  const noteTitle = document.querySelector(".note-title");
+  title.innerText = noteTitle.value;
   const content = document.createElement("div");
   content.classList.add("card-content");
-  content.innerText = "...";
+  const noteContent = document.querySelector(".note-content")
+  content.innerText = noteContent.value;
+
+  card.addEventListener("click", (e) => {
+    createNoteEditor(e);
+  });
+
   card.appendChild(date);
   card.appendChild(title);
   card.appendChild(content);
   const main = document.querySelector("main");
   main.appendChild(card);
+
+  document.querySelector(".overlay-note-creator").remove();
   card.scrollIntoView();
 }
 
@@ -66,4 +148,12 @@ function toggleSelectedGroup(section) {
   const selectedGroup = document.querySelector(".group-selected");
   selectedGroup.classList.toggle("group-selected");
   section.classList.toggle("group-selected");
+}
+
+// Dynamically size textarea
+function autoGrow(element) {
+  console.log(element + " auto_grow activated!");
+  
+  element.style.height = "1px";
+  element.style.height = (25+element.scrollHeight)+"px";
 }
